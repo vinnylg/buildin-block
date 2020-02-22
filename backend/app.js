954020fd58872337be861d53feddf51483e869ff
Vinícius de Lima Gonçalves <viniciusslimaa@outlook.com>
2020-02-22 13:26:58 -0300

    ajust backend to connect with db and some configs

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mongoose = require('mongoose')
+var debug = require('debug')('backend:db')
+
+mongoose.connect('mongodb://localhost/bb')
+
+const db = mongoose.connection
+db.on('error', (error) => debug(error) )
+db.once('open', () => debug('connected'))
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -20,7 +28,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
